9d2f75b45d806a6df0bfe2377f8e16ddc418e349
arionct <arionct@bu.edu>
2024-09-11 17:22:08 -0400

    removed unnecessary lines

--- polynomial.py
+++ polynomial.py
@@ -53,6 +53,4 @@ class Mul:
 
 poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
 print(poly)
-
-poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
 print(poly.evaluate(-1))
